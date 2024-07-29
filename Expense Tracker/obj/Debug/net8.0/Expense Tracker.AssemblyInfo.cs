253bcf7805bf5995563c57e376c4d0c26dd6c33a
Charan <charankulal0241@gmail.com>
2024-07-29 17:30:49 +0530

    Completed all the functionalities related to the Categories

--- Expense Tracker/obj/Debug/net8.0/Expense Tracker.AssemblyInfo.cs
+++ Expense Tracker/obj/Debug/net8.0/Expense Tracker.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Expense Tracker")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ee277046aa260905c06aa9b91ab4db3d2fef7900")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ef4fd99917626496afd6ff1f6d9762d2b2bd72f9")]
 [assembly: System.Reflection.AssemblyProductAttribute("Expense Tracker")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Expense Tracker")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
